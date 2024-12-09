6bc36794342eda8a20605af3a8b2da2c71703674
Achile Cornilleau <achile.cornilleau@etudiant.univ-lr.fr>
2024-12-09 08:34:34 +0100

    fix : delete room feature maintenance

---
+++ sfapp/migrations/Version20241209073151.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241209073151 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE acquisition_system ADD name VARCHAR(255) NOT NULL, ADD state VARCHAR(255) DEFAULT NULL, CHANGE room_id room_id INT DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_13C616225E237E06 ON acquisition_system (name)');
+        $this->addSql('ALTER TABLE room DROP description');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP INDEX UNIQ_13C616225E237E06 ON acquisition_system');
+        $this->addSql('ALTER TABLE acquisition_system DROP name, DROP state, CHANGE room_id room_id INT NOT NULL');
+        $this->addSql('ALTER TABLE room ADD description VARCHAR(255) DEFAULT NULL');
+    }
+}
