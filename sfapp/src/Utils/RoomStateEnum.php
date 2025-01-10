9616ceca12b319b0e05fbf43adf5411d0a680993
Achile Cornilleau <achile.cornilleau@etudiant.univ-lr.fr>
2025-01-10 09:15:13 +0100

    fix : translations

--- sfapp/src/Utils/RoomStateEnum.php
+++ sfapp/src/Utils/RoomStateEnum.php
@@ -13,23 +13,23 @@ namespace App\Utils;
 enum RoomStateEnum: string
 {
     /**
-     * Indicates that there is no data from the api for the moment or that the data are aberrants
+     * Indicates that there is no data from the api for the moment or that the data are aberrants (grey)
      */
     case NO_DATA = 'no data';
 
 
     /**
-     * Indicates that the room is in a stable condition with normal sensor readings.
+     * Indicates that the room is in a stable condition with normal sensor readings. (green)
      */
     case STABLE = 'stable';
 
     /**
-     * Indicates that the room is at risk due to abnormal sensor readings.
+     * Indicates that the room is at risk due to abnormal sensor readings. (yellow)
      */
     case AT_RISK = 'at risk';
 
     /**
-     * Indicates that the room is in a critical condition requiring immediate attention.
+     * Indicates that the room is in a critical condition requiring immediate attention. (red)
      */
     case CRITICAL = 'critical';
 }
--- sfapp/src/Utils/SensorStateEnum.php
+++ sfapp/src/Utils/SensorStateEnum.php
@@ -13,17 +13,17 @@ namespace App\Utils;
 enum SensorStateEnum: string
 {
     /**
-     * Indicates that the sensors are not linked to any acquisition system.
+     * Indicates that the sensors are not linked to any acquisition system. (grey)
      */
     case NOT_LINKED = 'not linked';
 
     /**
-     * Indicates that the sensors are linked and operational.
+     * Indicates that the sensors are linked and operational. (green)
      */
     case LINKED = 'linked';
 
     /**
-     * Indicates that the sensors are probably broken or disconnected and require maintenance.
+     * Indicates that the sensors are probably broken or disconnected and require maintenance. (red))
      */
     case NOT_WORKING = 'not working';
 }
